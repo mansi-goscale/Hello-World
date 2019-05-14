8e14f60af174147cda2a57a69b3e4c0b1ec604fc
Arya Stark of Winterfell <mansi.shah@getbaqala.com>
2019-05-14 19:51:27 +0530

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,6 @@
+git add ProjectTemplate
+git commit -m "Initial commit"
+git remote add origin https://github.com/mansi-goscale/Hello-World.git
+git push origin master
+mansi-goscale
+8b8c2d261f3e860a723dc5bd34f5141eb4adc539
